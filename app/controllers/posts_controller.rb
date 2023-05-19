468a9f3f0d75697ed5c4a5aa1f5d072fcebe163d
Prakhar Gupta <prakhar.gupta@kellogg.northwestern.edu>
2023-05-19 06:50:38 +0000

    syncpls

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,9 @@ class PostsController < ApplicationController
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
+    @post["place_id"] = params["post"]["place_id"]
     @post.save
-
+    redirect_to "/places/#{@post["place_id"]}"
   end
 
 end
